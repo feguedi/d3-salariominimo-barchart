de4c291d813f462b9d2709e103fffc984d6bb2a1
Eduardo Fernández <feguedi@yahoo.com.mx>
2021-12-24 06:52:52 -0600

    Barras de cada dato - Estilos de tooltip, overlay, x-axis y y-axis

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ async function getSalarioMinimo() {
 
 async function init() {
     try {
-        // const salarioMinimoData = []
         const width = 800
         const height = 400
         const padding = 30
@@ -42,6 +41,12 @@ async function init() {
             .attr('x', -300)
             .attr('y', 80)
             .text('Salario mínimo en México')
+        const salarioMaximo = d3.max(salarios)
+        const linearScale = d3.scaleLinear()
+            .domain([0, salarioMaximo])
+            .range([height, 0])
+
+        const salariosEscalados = salarios.map(salario => linearScale(salario))
 
         const xScale = d3.scaleLinear()
             .domain([d3.min(anios), d3.max(anios)])
@@ -65,6 +70,47 @@ async function init() {
             .attr('transform', `translate(${padding * 2}, 0)`)
             .attr('id', 'y-axis')
             .call(yAxis)
+
+        d3.select('svg')
+            .selectAll('rect')
+            .data(salariosEscalados)
+            .enter()
+            .append('rect')
+            .attr('data-date', (d, i) => new Date(data[i]['Año'], 1))
+            .attr('data-salario', (d, i) => Number(String(data[i]['Salario mínimo real']).replace(',', '.')) || 0)
+            .attr('class', 'bar')
+            .attr('x', (d, i) => xScale(anios[i]))
+            .attr('y', d => height - d)
+            .attr('width', barWidth)
+            .attr('height', d => d)
+            .attr('index', (d, i) => i)
+            .attr('transform', 'translate(60, 0)')
+            .on('mouseover', function(event, d) {
+                const i = this.getAttribute('index')
+
+                overlay.transition()
+                    .duration(0)
+                    .style('height', d + 'px')
+                    .style('width', barWidth + 'px')
+                    .style('opacity', 0.9)
+                    .style('left', i * barWidth + 0 + 'px')
+                    .style('top', height - d + 'px')
+                    .style('transform', 'translateX(60px)')
+
+                tooltip.transition()
+                    .duration(200)
+                    .style('opacity', 0.9)
+
+                tooltip.html(`${anios[i]}<br>$${salarios[i]} MXN`)
+                    .attr('data-date', data[i]['Año'])
+                    .style('left', `${i * barWidth + 30}px`)
+                    .style('top', `${height - 100}px`)
+                    .style('transform', 'translateX(60px)')
+            })
+            .on('mouseout', () => {
+                tooltip.transition().duration(200).style('opacity', 0)
+                overlay.transition().duration(200).style('opacity', 0)
+            })
 
     } catch (error) {
         console.error(error)
